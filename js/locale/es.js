38773e2a223d6856a6ab364f8209119ea5b43a21
lupa <lupa@piru>
2015-10-19 11:51:59 -0300

    Spanish translation added

---
+++ js/locale/es.js
@@ -0,0 +1,43 @@
+(function () {
+    'use strict';
+
+    window.i18n = {
+        /**
+         * Error message after submission
+         *
+         * "The account id provided is invalid"
+         */
+        invalidId: "La cuenta proporcionada no es válida.",
+
+        /**
+         * Form label
+         *
+         * "Your account ID:"
+         */
+        yourAcctId: "ID en GNUSocial:",
+
+        /**
+         * Form placeholder
+         *
+         * Please keep "@example.org" as a domain
+         * since it's a reserved domain name (http://www.iana.org/domains/reserved)
+         *
+         * "user@example.org"
+         */
+        idPlaceholder: "usuario@example.org",
+
+        /**
+         * Checkbox label
+         *
+         * "Share as a bookmark"
+         */
+        shareAsBookmark: "Compartir como marcador",
+
+        /**
+         * Submit button
+         *
+         * "Share"
+         */
+        submit: "Compartir"
+    };
+}());
